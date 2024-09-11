57cb5cb0b9fad98358a5b8cd7d9bcc5fe8471898
Rohan sahu <rohans.technorizen@gmail.com>
2024-09-11 19:26:22 +0530

    sales

--- src/screens/profile/Sales.js
+++ src/screens/profile/Sales.js
@@ -11,11 +11,12 @@ import { useFocusEffect } from '@react-navigation/native'
 import { DOMAIN } from '../../services/Config'
 import OrderSalesDetails from '../../components/OrderSalesDetails'
 import Upside from "../../assets/svg/upside.svg"
+import Shipment from "../../assets/svg/shipment.svg"
 import Theme from '../../theme'
 import { hp } from '../../utils/Constant'
 import MyButton from '../../elements/MyButton'
 
-
+import Heart from "../../assets/svg/Heart2.svg"
 const Sales = ({ navigation }) => {
     const userDetails = useSelector((state) => state?.user?.user)
     const [show, setShow] = useState("wind")
@@ -80,10 +81,14 @@ const Sales = ({ navigation }) => {
             })
     }
 
-    const _add_card = () => {
-
+    const _add_card = (item) => {
+      
+            setOpen(true)
+            setData(item)
     }
 
+
+    console.log('purchase',purchase);
     return (
         <View style={{ flex: 1, backgroundColor: "#fff" }}>
             <MyStatusBar backgroundColor={"transparent"} barStyle={"dark-content"} />
@@ -164,7 +169,7 @@ const Sales = ({ navigation }) => {
                             })} */}
 
 
-                            <FlatList
+                        {show === 'inprogress' &&    <FlatList
                                 numColumns={2}
                                 data={purchase}
                                 renderItem={({ item, index }) => (
@@ -246,11 +251,13 @@ const Sales = ({ navigation }) => {
                                                     }} resizeMode='cover' />
                                                     </TouchableOpacity>
 
-                                                    <MyButton loading={loading} onPress={_add_card} textStyle={{ fontSize: 18, fontWeight: "700", fontFamily: Theme.FONT_FAMILY_SEMIBOLD }} style={{ borderRadius: 15, marginVertical: 10, width: "100%", alignSelf: "center", marginTop: 20, }} title={"View Pickup Point"} />
+                                                    <MyButton loading={loading} onPress={()=>{_add_card(item)}} textStyle={{ fontSize: 18, fontWeight: "700", fontFamily: Theme.FONT_FAMILY_SEMIBOLD }} style={{ borderRadius: 15, marginVertical: 10, width: "100%", alignSelf: "center", marginTop: 20, }} title={"View Pickup Point"} />
 
                                                 </View>
 
-                                                <Pressable style={{ alignSelf: 'center', marginTop: 5 }}>
+                                                <Pressable
+                                               
+                                                style={{ alignSelf: 'center', marginTop: 5 }}>
                                                     <Upside />
                                                 </Pressable>
 
@@ -260,7 +267,136 @@ const Sales = ({ navigation }) => {
 
 
                                 )}
-                            />
+                            />}
+
+{show === 'wind' &&
+ <FlatList  
+ numColumns={2}
+ data={purchase}
+ renderItem={({item,index})=>(
+     <Pressable
+     onPress={() => {
+        setOpen(true)
+        setData(item)}}
+
+     key={index} style={{
+         width: "46%",
+         borderRadius: 12,
+         shadowOpacity: 0.8,
+         shadowRadius: 2,
+         shadowOffset: {
+             height: 0,
+             width: 0
+         },
+         backgroundColor: "#fff",
+         elevation: 5,
+         margin: 5,
+         marginBottom: 15
+     }}>
+     <View style={{
+         borderRadius: 12,
+         backgroundColor: "#fff",
+         height:150,
+         overflow: "hidden"
+     }}>
+         <Image source={{ uri: item?.product_images?.[0]?.image }} style={{
+             width: "100%", height: "100%", borderTopLeftRadius: 12,
+             borderTopRightRadius: 12,
+         }} />
+     </View>
+     <View style={{ justifyContent: "center", margin: 5, backgroundColor: "#fff", padding: 8 }}>
+     <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+
+         <Text style={{ color: "#04CFA4", fontSize: 14, fontWeight: '700' }}>
+             € {item?.price}
+         </Text >
+         <Heart />
+     </View>
+         <Text style={{ color: "#000",fontWeight:'600',fontSize:12 }}>
+             {item?.title?.substring(0,20)}
+         </Text >
+         <Text style={{ color: "#949494",fontWeight:'600',fontSize:12 }}>
+             {item?.description?.substring(0,25)}
+         </Text >
+     </View>
+ </Pressable>
+ )}
+ />
+                                    }
+{show === 'Complete' &&
+ <FlatList  
+ numColumns={2}
+ data={purchase}
+ renderItem={({item,index})=>(
+    <TouchableOpacity
+    onPress={() => setExpandable(Expandable =>!Expandable)}
+
+    key={index} style={{
+        width: '98%',
+        borderRadius: 12,
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+            height: 0,
+            width: 0
+        },
+        backgroundColor: "#fff",
+        elevation: 5,
+        margin: 5,
+        marginBottom: 15
+        ,
+
+        padding: 10
+    }}>
+    <View style={{
+        flexDirection: 'row',
+
+
+        width: '100%',
+        height: hp(10),
+        alignItems: 'center', justifyContent: 'space-between'
+    }}>
+        <View style={{
+            borderRadius: 12,
+            backgroundColor: "#fff",
+            height:60, width: 90
+        }}>
+            <Image source={{ uri: item?.product_images[0].image }} style={{
+                width:60, height: 60, borderRadius: 12,
+
+            }} resizeMode='cover' />
+        </View>
+        <View style={{ backgroundColor: "#fff", width: '55%',  }}>
+
+           
+
+
+            <Text style={{ color: "#000", fontWeight: '600', fontSize: 12, marginTop: 10 }}>
+                {item?.title?.substring(0, 20)}
+            </Text >
+            <View style={{flexDirection:'row',alignItems:'center'}}>
+<Shipment  />
+            <Text style={{ color: "#C3C6C9", fontWeight: '500', fontSize: 12,marginLeft:5 }}>
+            Shipment
+            </Text>
+            </View>
+            <Text style={{ color: "#C3C6C9", fontWeight: '500', fontSize: 12, marginTop: 5 }}>
+            Completed on Dec 14.
+            </Text >
+        </View>
+       
+
+        <Text style={{ color: "#04CFA4", fontSize: 14, fontWeight: '700' }}>
+                € {item?.price}
+            </Text>
+
+    </View>
+
+   
+</TouchableOpacity>
+ )}
+ />
+                                    }
                         </View>
                 }
             </ScrollView>
